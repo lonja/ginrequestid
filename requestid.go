be91a87195d4a1e03947517ce721514d9d51dfc2
Leonid Shylin <lonja007@gmail.com>
2018-08-02 18:57:17 +0300

    Update names consider to Go code style

--- requestid.go
+++ requestid.go
@@ -12,19 +12,19 @@ import (
 	"github.com/satori/go.uuid"
 )
 
-func RequestId() gin.HandlerFunc {
+func RequestID() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Check for incoming header, use it if exists
 		requestID := c.Request.Header.Get("X-Request-Id")
 
 		// Create request id with UUID4
 		if requestID == "" {
-			uuid4, _ := uuid.NewV4()
+			uuid4 := uuid.NewV4()
 			requestID = uuid4.String()
 		}
 
 		// Expose it for use in the application
-		c.Set("RequestId", requestID)
+		c.Set("request_id", requestID)
 
 		// Set X-Request-Id header
 		c.Writer.Header().Set("X-Request-Id", requestID)
